7edb64c3cffc8052f5b19d794d8f6358170397ae
KetanC1211 <ketanchindarkar12@gmail.com>
2022-08-31 12:49:38 +0530

    menu buttons

--- Person.js
+++ Person.js
@@ -17,126 +17,151 @@ class Person{
 const prompt = require('prompt-sync')();
 //contactlist Array to save new contacts
 let contactList = new Array();
-function checkName(name){
+let t = true;
+while (t) {
+    let choice = prompt("\t--Menu-- \n1. Create Contact \n2. Disply Contacts \n3. Edit Contact \n4. Exit");
+    if (choice === "1") {
+        addContact();
+    }
+    if (choice === "2") {
+        displaycontact();
+    }
+    if (choice === "3") {
+        t = false;
+    }
+    if (choice === "4") {
+        t = false;
+    }
+    if (choice != 1 && choice != 2 && choice != 3) {
+        console.log("Enter a Valid input")
+    }
+}
+function addContact(){
+    function checkName(name){
     let namePattern = RegExp('[A-Z]{1}[a-z]{2,}')
     if(namePattern.test(name)){
         return false;
     }else{
         return true;
     }
-}
-function checkAddress(address){
+    }
+    function checkAddress(address){
     let addPattern = RegExp('[a-zA-Z0-9\s]{3,}')
     if(addPattern.test(address)){
         return false;
     }else{
         return true;
     }
-}
+    }
 
-function checkPhoneNumber(pNum){
+    function checkPhoneNumber(pNum){
     let pNumPattern = RegExp('[5-9]{1}[0-9]{9}')
     if(pNumPattern.test(pNum)){
         return false;
     }else{
         return true;
     }
-}
-function checkEmail(email){
+    }
+    function checkEmail(email){
     let pNumPattern = RegExp('[a-z]{1,}([-+_])?([.][a-zA-Z0-9]{2,})?([A-Za-z0-9]{1,})?@[a-z0-9]{1,}.[a-z]{2,3}(.[a-z]{2,3})?')
     if(pNumPattern.test(email)){
         return false;
     }else{
         return true;
     }
-}
-function checkZip(zip){
+    }
+    function checkZip(zip){
     let zipPattern = RegExp('^[0-9]{6}$')
     if(zipPattern.test(zip)){
         return false;
     }else{
         return true;
     }
-}
+    }
 
-let t=true
-let fName;
-while(t){
+    t=true
+    let fName;
+    while(t){
     fName = prompt("Enter First name");
     t = checkName(fName);
     if(t==true){
         console.log("Invalid Input");
     }
-}
-let lastName;
-t=true;
-while(t){
+    }
+    let lastName;
+    t=true;
+    while(t){
     lastName = prompt("Enter Last name");
     t = checkName(lastName);
     if(t==true){
         console.log("Invalid Input");
     }
-}
-let address;
-t=true;
-while(t){
+    }
+    let address;
+    t=true;
+    while(t){
     address = prompt("Enter Address ");
     t = checkAddress(address);
     if(t==true){
         console.log("Invalid Input");
     }
-}
-let city;
-t=true;
-while(t){
+    }
+    let city;
+    t=true;
+    while(t){
     city = prompt("Enter City name");
     t = checkName(city);
     if(t==true){
         console.log("Invalid Input");
     }
-}
-let state;
-t=true;
-while(t){
+    }
+    let state;
+    t=true;
+    while(t){
     state = prompt("Enter State Name");
     t = checkName(state);
     if(t==true){
         console.log("Invalid Input");
     }
-}
-// let zip = 400156;
-let zip;
-t=true;
-while(t){
+    }
+    // let zip = 400156;
+    let zip;
+    t=true;
+    while(t){
     zip = prompt("Enter Zip");
     t = checkZip(zip);
     if(t==true){
         console.log("Invalid Input");
     }
-}
-let phoneNumber;
-t=true;
-while(t){
+    }
+    let phoneNumber;
+    t=true;
+    while(t){
     phoneNumber = prompt("Enter PhoneNumber Name");
     t = checkPhoneNumber(phoneNumber);
     if(t==true){
         console.log("Invalid Input");
     }
-}
-let email;
-t=true;
-while(t){
+    }
+    let email;
+    t=true;
+    while(t){
     email = prompt("Enter Email Name");
     t = checkEmail(email);
     if(t==true){
         console.log("Invalid Input");
     }
+    }
+    let obj = new Person(fName,lastName,address,city,state,zip,phoneNumber,email);
+    contactList.push(obj);
+    let Ketan = new Person('Ketan','Chindarkar','Hill Road','Mumbai','Maharashtra','400015','9967945885','ketan12@gmail.com');
+    let Yash = new Person('Yash','Ghade','Farm House','Panji','Goa','404715','7897894851','yash@gmail.com');
+    contactList.push(Ketan);
+    contactList.push(Yash);
 }
-let obj = new Person(fName,lastName,address,city,state,zip,phoneNumber,email);
-let Ketan = new Person('Ketan','Chindarkar','Hill Road','Mumbai','Maharashtra','400015','9967945885','ketan12@gmail.com');
-let Yash = new Person('Yash','Ghade','Farm House','Panji','Goa','404715','7897894851','yash@gmail.com');
-contactList.push(Ketan);
-contactList.push(Yash);
-contactList.push(obj);
-console.log(contactList);
+
+function displaycontact(){
+    console.log(contactList);
+}
+
 
